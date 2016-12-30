f0d7795a546fdca8c007008740773f08f438f930
nabijaczleweli <nabijaczleweli@gmail.com>
2016-12-31 00:14:24 +0100

    Handle x- prefixed encodings as well

--- src/util/content_encoding.rs
+++ src/util/content_encoding.rs
@@ -1,6 +1,6 @@
 use brotli2::stream::{CompressMode as BrotliCompressMode, CompressParams as BrotliCompressParams};
 use flate2::write::{DeflateEncoder, GzEncoder};
-use flate2::{Compression as Flate2Compression};
+use flate2::Compression as Flate2Compression;
 use bzip2::Compression as BzCompression;
 use iron::headers::{QualityItem, Encoding};
 use brotli2::write::BrotliEncoder;
@@ -10,10 +10,10 @@ use std::io::Write;
 
 lazy_static! {
     /// The list of content encodings we handle.
-    pub static ref SUPPORTED_ENCODINGS: Vec<Encoding> = vec![Encoding::Gzip,
-                                                             Encoding::Deflate,
-                                                             Encoding::EncodingExt("br".to_string()),
-                                                             Encoding::EncodingExt("bzip2".to_string())];
+    pub static ref SUPPORTED_ENCODINGS: Vec<Encoding> = {
+        let es = vec![Encoding::Gzip, Encoding::Deflate, Encoding::EncodingExt("br".to_string()), Encoding::EncodingExt("bzip2".to_string())];
+        [es.clone(), es.into_iter().map(|e| Encoding::EncodingExt(format!("x-{}", e))).collect()].into_iter().flat_map(|e| e.clone()).collect()
+    };
 }
 
 
@@ -30,8 +30,10 @@ pub fn encode_str(dt: &str, enc: &Encoding) -> Option<Vec<u8>> {
         Encoding::Deflate => encode_deflate(dt),
         Encoding::EncodingExt(ref e) => {
             match &e[..] {
-                "br" => encode_brotli(dt),
-                "bzip2" => encode_bzip2(dt),
+                "x-gzip" => encode_gzip(dt),
+                "x-deflate" => encode_deflate(dt),
+                "br" | "x-br" => encode_brotli(dt),
+                "bzip2" | "x-bzip2" => encode_bzip2(dt),
                 _ => None,
             }
         }
