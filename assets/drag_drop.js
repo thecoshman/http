db6dc35d531a4a73aa5830f6cb52584afb92e627
nabijaczleweli <nabijaczleweli@gmail.com>
2017-01-28 00:33:08 +0100

    Refresh after all files are uploaded
    Closes #52

--- assets/drag_drop.js
+++ assets/drag_drop.js
@@ -4,14 +4,15 @@ window.addEventListener("load", () => {
   let body = document.getElementsByTagName("body")[0];
 
   body.addEventListener("dragover", (ev) => {
-    if(SUPPORTED_TYPES.find(el => ev.dataTransfer.types.contains(el)))
+    if(SUPPORTED_TYPES.find(el => (ev.dataTransfer.types.contains || ev.dataTransfer.types.includes).call(ev.dataTransfer.types, el)))
       ev.preventDefault();
   });
 
   body.addEventListener("drop", (ev) => {
-    if(SUPPORTED_TYPES.find(el => ev.dataTransfer.types.contains(el))) {
+    if(SUPPORTED_TYPES.find(el => (ev.dataTransfer.types.contains || ev.dataTransfer.types.includes).call(ev.dataTransfer.types, el))) {
       ev.preventDefault();
 
+      let remaining_files = ev.dataTransfer.files.length;
       let url = document.URL;
       if(url[url.length - 1] != "/")
         url += "/";
@@ -19,7 +20,11 @@ window.addEventListener("load", () => {
       for(let i = ev.dataTransfer.files.length - 1; i >= 0; --i) {
         let file = ev.dataTransfer.files[i];
         let request = new XMLHttpRequest();
-        request.open('PUT', url + file.name);
+        request.addEventListener("loadend", (e) => {
+          if(--remaining_files === 0)
+            window.location.reload();
+        });
+        request.open("PUT", url + file.name);
         request.send(file);
       }
     }
