7a51adbb6bac8a4825ead229776fcb9a7945e9c3
nabijaczleweli <nabijaczleweli@gmail.com>
2019-09-15 07:21:52 +0200

    Properly hide symlinks when disabled

--- src/ops/webdav.rs
+++ src/ops/webdav.rs
@@ -2,7 +2,8 @@
 //! https://github.com/tylerwhall/hyperdav-server/blob/415f512ac030478593ad389a3267aeed7441d826/src/lib.rs
 
 
-use self::super::super::util::{CommaList, Depth, file_time_modified, file_time_created, is_actually_file, html_response, file_length, file_binary, ERROR_HTML};
+use self::super::super::util::{CommaList, Depth, file_time_modified, file_time_created, is_actually_file, is_descendant_of, html_response, file_length,
+                               file_binary, ERROR_HTML};
 use xml::{EmitterConfig as XmlEmitterConfig, ParserConfig as XmlParserConfig};
 use xml::reader::{EventReader as XmlReader, XmlEvent as XmlREvent};
 use xml::writer::{EventWriter as XmlWriter, XmlEvent as XmlWEvent};
@@ -69,10 +70,15 @@ lazy_static! {
 
 impl HttpHandler {
     pub(super) fn handle_webdav_propfind(&self, req: &mut Request) -> IronResult<Response> {
-        let (req_p, _, url_err) = self.parse_requested_path(req);
+        let (req_p, symlink, url_err) = self.parse_requested_path(req);
 
         if url_err {
             return self.handle_invalid_url(req, "<p>Percent-encoding decoded to invalid UTF-8.</p>");
+        }
+
+        if !req_p.exists() || (symlink && !self.follow_symlinks) ||
+           (symlink && self.follow_symlinks && self.sandbox_symlinks && !is_descendant_of(&req_p, &self.hosted_directory.1)) {
+            return self.handle_nonexistant(req, req_p);
         }
 
 
@@ -96,8 +102,64 @@ impl HttpHandler {
              req_p.display(),
              depth);
 
-        let resp = generate_propfind(req.url.as_ref().as_str(), &req_p, &props, depth).expect("Couldn't write PROPFIND XML");
-        Ok(Response::with((status::MultiStatus, resp, "text/xml;charset=utf-8".parse::<Mime>().unwrap())))
+        match self.handle_webdav_propfind_write_output(req, req.url.as_ref().as_str().to_string(), &req_p, &props, depth).expect("Couldn't write PROPFIND XML") {
+            Ok(xml_resp) => Ok(Response::with((status::MultiStatus, xml_resp, "text/xml;charset=utf-8".parse::<Mime>().unwrap()))),
+            Err(resp) => resp,
+        }
+    }
+
+    fn handle_webdav_propfind_write_output(&self, req: &mut Request, url: String, path: &Path, props: &[OwnedName], depth: Depth)
+                                           -> Result<Result<Vec<u8>, IronResult<Response>>, XmlWError> {
+        let mut resp = vec![];
+
+        let mut xml_out = XmlWriter::new_with_config(&mut resp, DEFAULT_XML_EMITTER_CONFIG.clone());
+        xml_out.write(XmlWEvent::StartDocument {
+                version: XmlVersion::Version10,
+                encoding: Some("utf-8"),
+                standalone: None,
+            })?;
+        xml_out.write(XmlWEvent::start_element("D:multistatus").ns("D", "DAV:"))?;
+
+        handle_propfind_path(&mut xml_out, &url, &path, &props)?;
+
+        if path.metadata().expect("Failed to get requested file metadata").is_dir() {
+            if let Some(ir) = self.handle_webdav_propfind_path_recursive(req, &mut xml_out, url, &path, &props, depth)? {
+                return Ok(Err(ir));
+            }
+        }
+
+        xml_out.write(XmlWEvent::end_element())?;
+
+        Ok(Ok(resp))
+    }
+
+    fn handle_webdav_propfind_path_recursive<W: Write>(&self, req: &mut Request, out: &mut XmlWriter<W>, root_url: String, root_path: &Path,
+                                                       props: &[OwnedName], depth: Depth)
+                                                       -> Result<Option<IronResult<Response>>, XmlWError> {
+        if let Some(next_depth) = depth.lower() {
+            for f in root_path.read_dir().expect("Failed to read requested directory").map(|p| p.expect("Failed to iterate over requested directory")) {
+                let mut url = root_url.clone();
+                if !url.ends_with('/') {
+                    url.push('/');
+                }
+                url.push_str(f.file_name().to_str().expect("Filename not UTF-8"));
+
+                let mut path = f.path();
+                let mut symlink = false;
+                while let Ok(newlink) = path.read_link() {
+                    symlink = true;
+                    path = newlink;
+                }
+
+                if !(!path.exists() || (symlink && !self.follow_symlinks) ||
+                     (symlink && self.follow_symlinks && self.sandbox_symlinks && !is_descendant_of(&path, &self.hosted_directory.1))) {
+                    handle_propfind_path(out, &url, &path, props)?;
+                    self.handle_webdav_propfind_path_recursive(req, out, url, &path, props, next_depth)?;
+                }
+            }
+        }
+
+        Ok(None)
     }
 
     pub(super) fn handle_webdav_proppatch(&self, req: &mut Request) -> IronResult<Response> {
@@ -129,28 +191,6 @@ impl HttpHandler {
     }
 }
 
-
-fn generate_propfind(url: &str, path: &Path, props: &[OwnedName], depth: Depth) -> Result<Vec<u8>, XmlWError> {
-    let mut resp = vec![];
-
-    let mut xml_out = XmlWriter::new_with_config(&mut resp, DEFAULT_XML_EMITTER_CONFIG.clone());
-    xml_out.write(XmlWEvent::StartDocument {
-            version: XmlVersion::Version10,
-            encoding: Some("utf-8"),
-            standalone: None,
-        })?;
-    xml_out.write(XmlWEvent::start_element("D:multistatus").ns("D", "DAV:"))?;
-
-    handle_propfind_path(&mut xml_out, url, &path, &props)?;
-
-    if path.metadata().expect("Failed to get requested file metadata").is_dir() {
-        handle_propfind_path_recursive(&mut xml_out, url, &path, &props, depth)?;
-    }
-
-    xml_out.write(XmlWEvent::end_element())?;
-
-    Ok(resp)
-}
 
 fn parse_propfind(req: &mut Request) -> Result<Vec<OwnedName>, String> {
     #[derive(Debug, Copy, Clone, Hash, PartialOrd, Ord, PartialEq, Eq)]
@@ -186,25 +226,6 @@ fn parse_propfind(req: &mut Request) -> Result<Vec<OwnedName>, String> {
             (st, ev) => return Err(format!("Unexpected event {:?} during state {:?}", ev, st)),
         }
     }
-}
-
-fn handle_propfind_path_recursive<W: Write>(out: &mut XmlWriter<W>, root_url: &str, root_path: &Path, props: &[OwnedName], depth: Depth)
-                                            -> Result<(), XmlWError> {
-    if let Some(next_depth) = depth.lower() {
-        for f in root_path.read_dir().expect("Failed to read requested directory").map(|p| p.expect("Failed to iterate over requested directory")) {
-            let mut url = root_url.to_string();
-            if !url.ends_with('/') {
-                url.push('/');
-            }
-            url.push_str(f.file_name().to_str().expect("Filename not UTF-8"));
-
-            let path = f.path();
-            handle_propfind_path(out, &url, &path, props)?;
-            handle_propfind_path_recursive(out, &url, &path, props, next_depth)?;
-        }
-    }
-
-    Ok(())
 }
 
 fn handle_propfind_path<W: Write>(out: &mut XmlWriter<W>, url: &str, path: &Path, props: &[OwnedName]) -> Result<(), XmlWError> {
