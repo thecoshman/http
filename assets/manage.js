f271b2713e3eca4a9ee23992977ef32c8a5c6dc3
nabijaczleweli <nabijaczleweli@gmail.com>
2019-10-22 07:56:16 +0100

    Rename edit_filename to rename. Handle renaming in desktop web ui

--- assets/manage.js
+++ assets/manage.js
@@ -6,6 +6,7 @@ window.addEventListener("load", function() {
   let new_directory_filename_input = null;
 
   let delete_file_links = document.getElementsByClassName("delete_file_icon");
+  let rename_links = document.getElementsByClassName("rename_icon");
 
 
   new_directory_line.addEventListener("click", function(ev) {
@@ -16,18 +17,11 @@ window.addEventListener("load", function() {
       new_directory_filename_input.focus();
     }
 
-    if(new_directory_filename_input === null) {
-      new_directory_filename_cell.innerHTML = "<input type=\"text\"></input>";
-      new_directory_filename_input = new_directory_filename_cell.children[0];
-
-      new_directory_filename_input.addEventListener("keypress", function(ev) {
-        if(ev.keyCode === 13)  // Enter
-          create_new_directory(new_directory_filename_input.value, new_directory_status_output);
+    if(new_directory_filename_input === null)
+      new_directory_filename_input = make_filename_input(new_directory_filename_cell, "", function() {
+        create_new_directory(new_directory_filename_input.value, new_directory_status_output);
       });
-
-      new_directory_filename_input.focus();
-    }
-  });
+  }, true);
 
   for(let i = delete_file_links.length - 1; i >= 0; --i) {
     let link = delete_file_links[i];
@@ -40,27 +34,83 @@ window.addEventListener("load", function() {
     });
   }
 
+  let first_rename_onclick = function(link, first_onclick, ev) {
+    ev.preventDefault();
+
+    let line = link.parentElement.parentElement;
+    let filename_cell = line.children[1];
+    let original_name = filename_cell.innerText;
+    let new_name_input = make_filename_input(filename_cell, original_name, function() {
+      rename(original_name, new_name_input.value, link);
+    });
+
+    link.removeEventListener("click", first_onclick);
+    link.addEventListener("click", function() {
+      new_name_input.focus();
+    });
+  };
+  for(let i = rename_links.length - 1; i >= 0; --i) {
+    let link = rename_links[i];
+
+    let first_onclick = function(ev) {
+      first_rename_onclick(link, first_onclick, ev);
+    };
+    link.addEventListener("click", first_onclick);
+  }
+
+
+  function make_filename_input(input_container, initial, callback) {
+    input_container.innerHTML = "<input type=\"text\"></input>";
+    let input_elem = input_container.children[0];
+    input_elem.value = initial;
+
+    input_elem.addEventListener("keypress", function(ev) {
+      if(ev.keyCode === 13)  // Enter
+        callback();
+    });
+
+    input_elem.focus();
+
+    return input_elem;
+  };
 
   function create_new_directory(fname, status_out) {
     let req_url = window.location.origin + window.location.pathname;
     if(!req_url.endsWith("/"))
       req_url += "/";
-    req_url += encodeURIComponent(fname);
+    req_url += fname;
 
     make_request("MKCOL", req_url, status_out);
   };
 
-  function make_request(verb, url, status_out) {
+  function rename(fname_from, fname_to, status_out) {
+    let root_url = window.location.origin + window.location.pathname;
+    if(!root_url.endsWith("/"))
+      root_url += "/";
+
+    if(fname_from.endsWith("/"))
+      fname_from = fname_from.substr(0, fname_from.length - 1);
+    if(fname_to.endsWith("/"))
+      fname_to = fname_to.substr(0, fname_to.length - 1);
+
+    make_request("MOVE", root_url + fname_from, status_out, function(request) {
+      request.setRequestHeader("Destination", root_url + fname_to);
+    });
+  };
+
+  function make_request(verb, url, status_out, request_modifier) {
     let request = new XMLHttpRequest();
-    request.addEventListener("loadend", function(ev) {
+    request.addEventListener("loadend", function() {
       if(request.status >= 200 && request.status < 300)
         window.location.reload();
       else {
-        status_out.innerHTML = request.status + " " + request.statusText + " — " + request.response;
+        status_out.innerHTML = request.status + " " + request.statusText + (request.response ? " — " : "") + request.response;
         status_out.classList.add("has-log");
       }
     });
     request.open(verb, url);
+    if(request_modifier)
+      request_modifier(request);
     request.send();
   };
 });
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -78,10 +78,10 @@ lazy_static! {
             Cow::Owned(format!("data:{};base64,{}",
                                get_mime_type_str("png").unwrap(),
                                Base64Display::with_config(&include_bytes!("../../assets/icons/delete_file_icon.png")[..], base64::STANDARD))));
-        ass.insert("edit_filename_icon",
+        ass.insert("rename_icon",
             Cow::Owned(format!("data:{};base64,{}",
                                get_mime_type_str("png").unwrap(),
-                               Base64Display::with_config(&include_bytes!("../../assets/icons/edit_filename_icon.png")[..], base64::STANDARD))));
+                               Base64Display::with_config(&include_bytes!("../../assets/icons/rename_icon.png")[..], base64::STANDARD))));
         ass.insert("date", Cow::Borrowed(include_str!("../../assets/date.js")));
         ass.insert("manage", Cow::Borrowed(include_str!("../../assets/manage.js")));
         ass.insert("upload", Cow::Borrowed(include_str!("../../assets/upload.js")));
