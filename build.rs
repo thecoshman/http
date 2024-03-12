448f96f8d3548054fd414171dfe279e9d52a0c70
наб <nabijaczleweli@nabijaczleweli.xyz>
2024-03-12 14:04:00 +0100

    Flatten build.rs

--- build.rs
+++ build.rs
@@ -4,18 +4,8 @@ extern crate cc;
 
 
 fn main() {
-    embed_resources();
-    get_ioctl_data();
-}
-
-fn embed_resources() {
     embed_resource::compile("http-manifest.rc");
-}
 
-#[cfg(any(target_os = "windows", target_os = "macos"))]
-fn get_ioctl_data() {}
-
-#[cfg(not(any(target_os = "windows", target_os = "macos")))]
-fn get_ioctl_data() {
+    #[cfg(not(any(target_os = "windows", target_os = "macos")))]
     cc::Build::new().file("build-ioctl.c").define("_GNU_SOURCE", "1").compile("http-ioctl");
 }
